41bf75c6097647dc35a58deb740e07d23ee90fe7
SHRI <dhageshreya2008@gmail.com>
2026-08-15 11:09:41 +0530

    login

--- app/src/main/java/com/example/nextgen/CustomerFragments/ProfileFragment.java
+++ app/src/main/java/com/example/nextgen/CustomerFragments/ProfileFragment.java
@@ -10,13 +10,11 @@ import android.view.ViewGroup;
 
 import com.example.nextgen.R;
 
-public class ProfileFragment extends Fragment {
-
-
+public class ProfileFragment extends Fragment
+{
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile2, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_profile2, container, false);
+        return view;
     }
 }
