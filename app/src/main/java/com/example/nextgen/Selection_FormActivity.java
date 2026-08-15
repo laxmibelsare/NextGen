586786aceab486f7d400b4e9050436d9e96cb88d
laxmi <belsarelaxmi5@gmail.com>
2026-08-15 09:52:09 +0530

    Commit Selection_FormActivity.java

--- app/src/main/java/com/example/nextgen/Selection_FormActivity.java
+++ app/src/main/java/com/example/nextgen/Selection_FormActivity.java
@@ -1,6 +1,9 @@
 package com.example.nextgen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,32 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Selection_FormActivity extends AppCompatActivity {
 
+    TextView CustomerArrow, VendorArrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_selection_form);
+
+        CustomerArrow = findViewById(R.id.tvCustomerArrow);
+        VendorArrow = findViewById(R.id.tvVendorArrow);
+
+        CustomerArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Selection_FormActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
+        VendorArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Selection_FormActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
