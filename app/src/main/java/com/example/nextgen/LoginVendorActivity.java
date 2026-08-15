d8df9ab1c6d4fa9fe68953891722216a8a23b1ce
SHRI <dhageshreya2008@gmail.com>
2026-08-15 16:15:06 +0530

    Shreyaaa

--- app/src/main/java/com/example/nextgen/LoginVendorActivity.java
+++ app/src/main/java/com/example/nextgen/LoginVendorActivity.java
@@ -3,15 +3,16 @@ package com.example.nextgen;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class LoginVendorActivity extends AppCompatActivity
 {
 
-    Button btnLogin;
+    MaterialButton btnLogin;
 
     TextView SignUp,forgetPass;
 
@@ -52,7 +53,7 @@ public class LoginVendorActivity extends AppCompatActivity
             @Override
             public void onClick(View v)
             {
-                Intent i = new Intent(LoginVendorActivity.this, ForgetActivity.class);
+                Intent i = new Intent(LoginVendorActivity.this, ForgetVendorActivity.class);
                 startActivity(i);
 
             }
