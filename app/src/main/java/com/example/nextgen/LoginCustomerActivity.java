1443f58b0ea2ced62c9e397f1aee414799832744
SHRI <dhageshreya2008@gmail.com>
2026-08-15 16:39:28 +0530

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/nextgen/LoginCustomerActivity.java
+++ app/src/main/java/com/example/nextgen/LoginCustomerActivity.java
@@ -8,10 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginCustomerActivity extends AppCompatActivity {
+public class LoginCustomerActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_customer);
     }
